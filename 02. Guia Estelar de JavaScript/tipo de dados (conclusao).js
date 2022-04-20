7a3f520db8703362d81a40d3755a1b2b779bec6b
Maykel André <eumaykelandre@gmail.com>
2022-04-20 15:07:02 -0300

    Create tipo de dados (conclusao).js

---
+++ 02. Guia Estelar de JavaScript/tipo de dados (conclusao).js
@@ -0,0 +1,28 @@
+// Conforme o ECMAScript Standard (o padrão do orgão que padroniza o JavaScript) temos 9 tipos de dados organizados em 4 categorias: Primitive, Primitive Value, Structural e Structural Primitive. Os primitivos são: String, Number, Boolean, Undefined, Symbol, BigInt. Os estruturais são: Object e Function. Além destes, temos o primitivo estrutural que consiste apenas no dado null
+
+// >> Tipos de Dados <<
+
+// *Primitive / Primitive Value
+// *Structural
+// *Structural Primitive
+
+// >> Primitivos << Valores imutáveis, não sofre alterações
+
+// *String
+// *Number
+// *Boolean
+// *Undefined
+// *Symbol
+// *BigInt
+
+// >> Estruturais <<
+
+// > Object <
+
+// *Array
+// *Map
+// *Set
+// *Date
+// *...
+
+// > function <
