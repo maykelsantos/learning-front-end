484a5f074be9c9f296de2dee8950eadc39d1bad2
Maykel André <eumaykelandre@gmail.com>
2022-04-20 14:54:00 -0300

    Create sobre tipos de dados

---
+++ 02. Guia Estelar de JavaScript/tipo de dados (boolean).js
@@ -0,0 +1,7 @@
+// No JavaScript, um dado boolean pode ter apenas 2 valores, verdadeiro (true) e falso (false).
+
+console.log(true)
+console.log(false)
+
+console.log(undefined === null) // o resultado desse script é 'false'
+console.log(10 === (20 - 10)) // o resultado desse script é 'true'
---
+++ 02. Guia Estelar de JavaScript/tipo de dados (object).js
@@ -0,0 +1,9 @@
+// O tipo de dado Object é estrutural, contendo atributos, ou propriedades, e métodos, ou funcionalidades. Um objeto é denotado com os caracteres { }, atributos e métodos com nome: valor . Por exemplo { nome: "João", idade: 20 }.
+
+console.log({
+    name: `Maykel`, // name = propriedade; `Maykel` = valor
+    idade: 27,
+    andar: function() {
+        console.log(`andar`)
+    }
+})
