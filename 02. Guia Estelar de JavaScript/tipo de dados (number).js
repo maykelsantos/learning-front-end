dc4b37a3236834e95cd8c7e95d9e2a68848d5082
Maykel André <eumaykelandre@gmail.com>
2022-04-20 14:41:48 -0300

    Create tipo de dados (number).js

---
+++ 02. Guia Estelar de JavaScript/tipo de dados (number).js
@@ -0,0 +1,15 @@
+/*
+No JavaScript temos 4 tipos de números, os inteiros (por exemplo: 13), os reais ou float (por exemplo: 83.1), o Not a Number (NaN) e o infinito (Infinity).
+*/
+
+// números interios
+console.log(33)
+console.log(-984)
+
+// números reais
+console.log(122.034)
+console.log(-0.0001)
+
+// números inteiros e reais com expressões
+console.log(33 + (-984))
+console.log(122.034 / (-0.0001))
