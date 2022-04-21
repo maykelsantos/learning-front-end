5c2042cf441516e12b02ffb8babf590f53cc485c
Maykel André <eumaykelandre@gmail.com>
2022-04-20 22:01:56 -0300

    Create concatenando e interpolando variáveis.js

---
+++ 02. Guia Estelar de JavaScript/concatenando e interpolando variáveis.js
@@ -0,0 +1,19 @@
+// concatenando variáveis
+{
+let name, age, color
+name = `maykel`
+age = 27
+color = `green`
+
+console.log(`Meu nome é ` + name + ` tenho ` + age + ` anos, e minha cor preferida é ` + color)
+}
+
+// interpolando variáveis
+{
+let name, age, color
+name = `maykel`
+age = 27
+color = `green`
+
+console.log(`Meu nome é ${name} tenho ${age} anos e minha cor preferida é ${color}`)
+}
