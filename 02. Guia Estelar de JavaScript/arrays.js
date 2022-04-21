6f3ec87d8d728e5427a26f795e61a5ef905806b8
Maykel André <eumaykelandre@gmail.com>
2022-04-20 22:31:40 -0300

    Create arrays.js

---
+++ 02. Guia Estelar de JavaScript/arrays.js
@@ -0,0 +1,10 @@
+{
+    const animals = [
+        `Lion`,
+        `Monkey`,
+        `Cat`,
+    ]
+    console.log(animals) // acessa todos os valores dentro do array
+    console.log(animals[0]) // acessa valor específico dentro do array, de acordo com a posição dos dados.
+    console.log(animals[2])
+}
