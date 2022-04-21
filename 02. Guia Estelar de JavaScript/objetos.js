ce436bcf52d813639a35e6d827a72bd6123cb2dd
Maykel André <eumaykelandre@gmail.com>
2022-04-20 22:21:52 -0300

    Create objetos.js

---
+++ 02. Guia Estelar de JavaScript/objetos.js
@@ -0,0 +1,22 @@
+{
+const person = {
+    name: `Maykel`,
+    age: 27,
+    weigth: 81.6,
+    isAdmin: true
+}
+
+// declarando a variável do objeto, obtenho todos os dados do objeto
+console.log(person) 
+
+// para obter dados específicos do objeto
+console.log(person.name)
+
+console.log(person.age)
+
+console.log(person.weigth)
+
+console.log(person.isAdmin)
+
+console.log(`${person.name} tem ${person.age} anos de idade e seu peso equivale a ${person.weigth} kg.`)
+}
