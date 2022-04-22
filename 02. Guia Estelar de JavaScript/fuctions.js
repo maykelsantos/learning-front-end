07e1779e6de9ad1eb675861172c01b18dec18da2
Maykel André <eumaykelandre@gmail.com>
2022-04-21 21:19:42 -0300

    Create fuctions.js

---
+++ 02. Guia Estelar de JavaScript/fuctions.js
@@ -0,0 +1,20 @@
+// criar aplicativo de frases motivacionais
+
+{
+    console.log(`Estudar é muito bom`)
+    console.log(`Paciência e persistência`)
+    console.log(`Revisão é mãe do aprendizado`)
+
+// declaration - declaração da função
+// function statement
+    function createPhrases() {
+        console.log(`Estudar é muito bom`)
+        console.log(`Paciência e resistência`)
+        console.log(`Revisão é mãe do aprendizado`)
+    }
+
+// executar a função
+// rodar , chamar, invocar
+// execute, run, call, invoke
+    createPhrases()
+}
