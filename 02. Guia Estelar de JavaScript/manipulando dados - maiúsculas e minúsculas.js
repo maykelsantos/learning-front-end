bd92560c1b805a1c2683bb8ad7d870dbfff7c4b8
Maykel André <eumaykelandre@gmail.com>
2022-04-26 11:05:53 -0300

    Create manipulando dados - maiúsculas e minúsculas.js

---
+++ 02. Guia Estelar de JavaScript/manipulando dados - maiúsculas e minúsculas.js
@@ -0,0 +1,17 @@
+{
+    // transformando letras minúsculas em maiúsculas
+    let word = `programar é muito bacana`
+    word.toUpperCase() // resultado `PROGRAMAR É MUITO BACANA`
+
+    // para mostrar na tela
+    console.log(word.toUpperCase())
+}
+
+{
+    // transformando letras maiúsculas em minúsculas
+    let word = `PROGRAMAR É MUITO BACANA`
+    word.toLowerCase() // resultado `programar é muito bacana`
+
+    // para mostrar na tela
+    console.log(word.toLowerCase())
+}
