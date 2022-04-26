98dc4513bf2b59735438eb901178f06a45bb5612
Maykel André <eumaykelandre@gmail.com>
2022-04-26 10:35:13 -0300

    Create manipulando dados - string em número, número em string.js

---
+++ 02. Guia Estelar de JavaScript/manipulando dados - string em número, número em string.js
@@ -0,0 +1,19 @@
+{
+    // transformando uma string em um número
+    let string = `123`
+    console.log(Number(string)) // `console.log` serve somente para mostrar em tela a trasnformação
+
+    // transformando um número em uma string
+    let number = 123
+    console.log(String(number))
+}
+
+{
+    // trasnformando uma string em número
+    let string = `123`
+    Number(string)
+
+    // transformando um número em string
+    let number = 123
+    String(number)
+}
