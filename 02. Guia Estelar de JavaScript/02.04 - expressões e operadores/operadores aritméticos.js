e63eb9f0556694c2a9d8140a61f68ec108366ddc
Maykel André <eumaykelandre@gmail.com>
2022-04-26 21:29:32 -0300

    Create operadores aritméticos.js

---
+++ 02. Guia Estelar de JavaScript/02.04 - expressões e operadores/operadores aritméticos.js
@@ -0,0 +1,31 @@
+{
+    // multiplicação
+    {
+        console.log(3.2 * 5.5) // resultado `17.6`
+    }
+
+    // divisão
+    {
+        console.log(3.2 / 5.5) // resultado `0.5818181818181819`
+    }
+
+    // soma
+    {
+        console.log(3.2 + 5.5) // resultado `8.7`
+    }
+
+    // subtração
+    {
+        console.log(3.2 - 5.5) // resultado `-2.3`
+    }
+
+    // resto da divisão
+    {
+        console.log(3.2 % 5.5) // resultado `3.2`
+    }
+
+    // exponencial
+    {
+        console.log(3.2 ** 5.5) // resultado `600.239927187154`
+    }
+}
