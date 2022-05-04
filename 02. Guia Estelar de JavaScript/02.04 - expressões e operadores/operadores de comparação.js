d2959e8e7173d0e106768a708348728326f41fe7
Maykel André <eumaykelandre@gmail.com>
2022-05-04 08:48:41 -0300

    Create operadores de comparação.js

---
+++ 02. Guia Estelar de JavaScript/02.04 - expressões e operadores/operadores de comparação.js
@@ -0,0 +1,13 @@
+{
+    let one = 1
+    let two = 2
+
+    // == igual a [...]
+    console.log(two == 1) // o resultado é `false`
+    console.log(one == 1) // o resultado é `true`
+
+    // != diferente de [...]
+    console.log(one != two) // o resultado é `true`
+    console.log(one != 1) // o resultado é `false`
+    console.log(one != `1`) // o resultado é `false`
+}
