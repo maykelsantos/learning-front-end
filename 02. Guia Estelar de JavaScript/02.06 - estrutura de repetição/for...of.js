608d1cfdb4ba33f81b101f3f27d2c8c9e4b47086
Maykel André <eumaykelandre@gmail.com>
2022-05-10 11:11:56 -0300

    Create for...of.js

---
+++ 02. Guia Estelar de JavaScript/02.06 - estrutura de repetição/for...of.js
@@ -0,0 +1,16 @@
+{
+    let name = `Maykel`
+    let names = [`Maykel`, `Vitória`, `Miguel`]
+
+    for(let caractere of name) {
+        console.log(caractere)
+    }
+}
+{
+    let name = `Maykel`
+    let names = [`Maykel`, `Vitória`, `Miguel`]
+
+    for (let name of names) {
+        console.log(name)
+    }
+}
