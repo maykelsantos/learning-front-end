970808165151e193f2ed6353e51ba8e40b8516be
Maykel André <eumaykelandre@gmail.com>
2022-05-25 19:51:16 -0300

    update

---
+++ 04. Pilotando com a DOM/script-1.js
@@ -0,0 +1,7 @@
+const openModalButton = document.getElementById(`openModal`) 
+const modalWrapper = document.querySelector(`.modal-wrapper`)
+openModalButton.onclick = function() {
+    modalWrapper.
+    classList.
+    remove(`invisible`)
+}
